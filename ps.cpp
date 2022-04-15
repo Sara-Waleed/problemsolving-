fd3b4103b5dbd300a6ae97fc5c2fb0239bb50ae7
Sara Waleed Mohamed elsaid <92535228+Sara-Waleed@users.noreply.github.com>
2022-04-15 16:23:34 +0200

    Add files via upload

---
+++ ps.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+string p;
+cin>>p;
+//int len = s1.length();
+for (int i=0;i<p.length();i++){
+    if(p[i]!= "?"){
+        cout<<p<<endl;
+    }
+    else
+        p[i]==5;
+}
+
+
+
+
+    return 0;
+}
