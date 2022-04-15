c978e85bf9a35a4a0b48e4d91efa1452749b79dc
Sara Waleed Mohamed elsaid <92535228+Sara-Waleed@users.noreply.github.com>
2022-04-15 16:25:56 +0200

    Add files via upload

---
+++ seven.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+/*
+long long  n,A;
+cin>>n>>A;
+
+switch(n){
+ case 1:
+     cout<<to_string(A);
+     break;
+ case 2:
+    cout<<to_string(A/10);
+    break;
+ case 3:
+    cout<<to_string(A/100);
+    break;
+ case 4:
+    cout<<to_string(A/1000);
+    break;
+ case 5:
+    cout<<to_string(A/10000);
+    break;
+ case 6:
+    cout<<to_string(A/100000);
+    break;
+ case 7:
+    cout<<to_string(A/1000000);
+
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  int n;
+  cin>>n;
+  if(n==7){
+        cout<<"yes";
+    }
+
+
+else{
+    cout<<"no";
+
+}
+
+    return 0;
+}
+
