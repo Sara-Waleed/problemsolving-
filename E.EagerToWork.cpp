50a6803328a117adbb33943e863ff4be4eefcc70
Sara Waleed Mohamed elsaid <92535228+Sara-Waleed@users.noreply.github.com>
2022-05-28 10:08:07 +0200

    Add files via upload

---
+++ E.EagerToWork.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include<cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+   
+    int t;
+    cin>>t;
+    while(t--){
+   
+int h,u,r;
+cin>>h>>u>>r;
+double de=sqrt((h*h+u*u));
+   
+std::cout << std::fixed;
+    std::cout << std::setprecision(9);
+    std::cout << de;
+
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+       return 0;
+}
+    
